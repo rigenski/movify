ea68ee19b6722cfebee7f54d9b4764108cc5000f
Rigen Maulana <rygen.dev@gmail.com>
2023-10-21 16:54:08 +0700

    feat: added icon

--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -4,6 +4,7 @@ import MovieDetail from "./../screens/MovieDetail/MovieDetail";
 import Movies from "./../screens/Movies/Movies";
 import UserDetail from "./../screens/UserDetail/UserDetail";
 import Users from "./../screens/Users/Users";
+import { Image, View } from "react-native";
 
 const Tab = createBottomTabNavigator();
 const MoviesStack = createStackNavigator();
@@ -46,12 +47,28 @@ const TabNavigator = () => (
     <Tab.Screen
       name="Movies"
       component={MoviesStackScreen}
-      options={{ headerShown: false }}
+      options={{
+        headerShown: false,
+        tabBarIcon: ({ color, size }) => (
+          <Image
+            source={require("./../assets/images/movies-icon.png")}
+            style={{ height: 32, width: 32 }}
+          />
+        ),
+      }}
     />
     <Tab.Screen
       name="Users"
       component={UserStackScreen}
-      options={{ headerShown: false }}
+      options={{
+        headerShown: false,
+        tabBarIcon: ({ color, size }) => (
+          <Image
+            source={require("./../assets/images/users-icon.png")}
+            style={{ height: 32, width: 32 }}
+          />
+        ),
+      }}
     />
   </Tab.Navigator>
 );
